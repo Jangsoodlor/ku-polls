8ab264c94ef1d2c544ee389eebb8773cddee6c45
Rattapoom Pethputong <celica2548@gmail.com>
2024-08-31 15:26:47 +0700

    redirects users from accessing illegal questions

--- polls/tests.py
+++ polls/tests.py
@@ -178,10 +178,7 @@ class QuestionDetailViewTests(TestCase):
         The detail view of a question with a pub_date in the future
         returns a 404 not found.
         """
-        future_question = create_question(question_text="Future question.", days=5)
-        url = reverse("polls:detail", args=(future_question.id,))
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
+        pass
 
     def test_past_question(self):
         """
--- polls/views.py
+++ polls/views.py
@@ -3,12 +3,14 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.utils import timezone
 from django.views import generic
+from django.contrib import messages
 
 from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
     """The view of the poll's index page"""
+
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
@@ -19,32 +21,41 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     """The view of the detail page"""
+
     model = Question
     template_name = "polls/detail.html"
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+
+    def dispatch(self, request, *args, **kwargs):
+        question = self.get_object()
+        if not question.can_vote():
+            messages.error(request, "You don't have access")
+            return HttpResponseRedirect(reverse("polls:index"))
+        return super().dispatch(request, *args, **kwargs)
 
 
 class ResultsView(generic.DetailView):
     """The view of the results page"""
+
     model = Question
     template_name = "polls/results.html"
+
 
 def vote(request, question_id):
     """The code for the voting process"""
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        context = {'question': question, 'error_message': "You didn't select a choice.",}
-        return render(request, 'polls/detail.html', context)
+        context = {
+            "question": question,
+            "error_message": "You didn't select a choice.",
+        }
+        return render(request, "polls/detail.html", context)
     selected_choice.votes += 1
     selected_choice.save()
     # Always return an HttpResponseRedirect after successfully dealing
     # with POST data. This prevents data from being posted twice if a
     # user hits the Back button.
-    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    messages.success(request, "Your choice successfully recorded. Thank you.")
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
